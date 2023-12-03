4b64d5a46ece969db87a11015f8b91c52105960c
06776 <hajdarapatrik11@outlook.com>
2023-12-03 14:12:52 +0100

    Create admin.js

---
+++ BackEnd/models/admin.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Név"],
+  },
+  email: {
+    type: String,
+    required: [true, "E-mail"],
+  },
+  password: {
+    type: String,
+    required: [true, "Jelszó"],
+    minLength: [10, "A jelszónak legalább 10 karakterből kell állnia"],
+    select: false,
+  },
+  description: {
+    type: String,
+  },
+  role: {
+    type: String,
+    default: "Owner",
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  resetPasswordToken: String,
+  resetPasswordTime: Date,
+});
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+adminSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+
+adminSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("Admin", adminSchema);
