1b9fa396d9ab2b69de2764d3394e0778bb9c6334
06776 <hajdarapatrik11@outlook.com>
2023-12-03 14:04:58 +0100

    Create user.js

---
+++ BackEnd/models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Kérjük, add meg a nevedet"],
+  },
+  email: {
+    type: String,
+    required: [true, "Kérjük, add meg az e-mail címedet"],
+  },
+  password: {
+    type: String,
+    required: [true, "Kérjük, adj meg egy jelszót"],
+    minLength: [8, "A jelszónak legalább 8 karakterből kell állnia"],
+    select: false,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  resetPasswordToken: String,
+  resetPasswordTime: Date,
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
