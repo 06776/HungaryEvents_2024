a30b3191769b9e49e09576dcf95c5b55b38970e4
06776 <hajdarapatrik11@outlook.com>
2023-12-03 16:09:08 +0100

    Modify auth.js

--- BackEnd/middleware/auth.js
+++ BackEnd/middleware/auth.js
@@ -1,3 +1,43 @@
 const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const Admin = require("../models/admin");
+
+exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new ErrorHandler("Please login to continue", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  req.user = await User.findById(decoded.id);
+
+  next();
+});
+
+exports.isOwner = catchAsyncErrors(async (req, res, next) => {
+  const { owner_token } = req.cookies;
+  if (!owner_token) {
+    return next(new ErrorHandler("Please login to continue", 401));
+  }
+
+  const decoded = jwt.verify(owner_token, process.env.JWT_SECRET_KEY);
+
+  req.owner = await Admin.findById(decoded.id);
+
+  next();
+});
+
+exports.isAdmin = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(`${req.user.role} can not access this resources!`)
+      );
+    }
+    next();
+  };
+};
