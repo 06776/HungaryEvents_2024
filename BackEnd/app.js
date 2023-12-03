438a8d250263d661308276b2fb093891016f2451
06776 <hajdarapatrik11@outlook.com>
2023-12-03 15:37:26 +0100

    app.js changes

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,18 +1,42 @@
 const express = require("express");
-const ErrorHandler = require("./utils/ErrorHandler");
+const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: [""],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
+
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "backend/config/.env",
+    path: "config/.env",
   });
 }
+
+const user = require("./controllers/user");
+const admin = require("./controllers/admin");
+const communityProgram = require("./controllers/communityProgram");
+const sight = require("./controllers/sight");
+const message = require("./controllers/message");
+
+app.use("/api/v2/user", user);
+app.use("/api/v2/message", message);
+app.use("/api/v2/admin", admin);
+app.use("/api/v2/communityProgram", communityProgram);
+app.use("/api/v2/sight", sight);
 
 app.use(ErrorHandler);
 
