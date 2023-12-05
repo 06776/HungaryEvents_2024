bb9cade7d9248593abb5a48b04badd9761bf49d5
06776 <hajdarapatrik11@outlook.com>
2023-12-05 12:15:47 +0100

    error.js modifications

--- BackEnd/middleware/error.js
+++ BackEnd/middleware/error.js
@@ -2,25 +2,25 @@ const ErrorHandler = require("../utils/ErrorHandler");
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.message = err.message || "Internal Server Error";
+  err.message = err.message || "Internal server Error";
 
   if (err.name === "CastError") {
-    const message = `Resource not found. Invalid: ${err.path}`;
+    const message = `Resources not found with this id.. Invalid ${err.path}`;
     err = new ErrorHandler(message, 400);
   }
 
   if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
     err = new ErrorHandler(message, 400);
   }
 
   if (err.name === "JsonWebTokenError") {
-    const message = `Json Web Token is invalid, Try again`;
+    const message = `Your url is invalid please try again letter`;
     err = new ErrorHandler(message, 400);
   }
 
   if (err.name === "TokenExpiredError") {
-    const message = `Json Web Token is Expired, Try again`;
+    const message = `Your Url is expired please try again letter!`;
     err = new ErrorHandler(message, 400);
   }
 
