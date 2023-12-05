1f437f5e58fe701172bfc19159da344424bbe145
06776 <hajdarapatrik11@outlook.com>
2023-12-05 12:44:47 +0100

    Changes in app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -7,17 +7,13 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: [""],
+    origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/test", (req, res) => {
-  res.send("Hello world!");
-});
-
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -28,15 +24,13 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 const user = require("./controllers/user");
 const admin = require("./controllers/admin");
-const communityProgram = require("./controllers/communityProgram");
-const sight = require("./controllers/sight");
+const program = require("./controllers/program");
 const message = require("./controllers/message");
 
 app.use("/api/v2/user", user);
 app.use("/api/v2/message", message);
 app.use("/api/v2/admin", admin);
-app.use("/api/v2/communityProgram", communityProgram);
-app.use("/api/v2/sight", sight);
+app.use("/api/v2/program", program);
 
 app.use(ErrorHandler);
 
